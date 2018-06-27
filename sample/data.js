e3213c62ec7f6f5844e5be66405259fbcba83edc
Masatoshi SEKI <m_seki@mva.biglobe.ne.jp>
2018-06-27 23:55:38 +0900

    table

---
+++ sample/data.js
@@ -0,0 +1,49 @@
+const th = {
+  th1: "th1",
+  th2: "th2",
+  th3: "th3",
+  th4: "th4",
+  th5: "th5",
+  th6: "th6",
+  th7: "th7",
+  th8: "th8",
+  th9: "th9",
+  th10: "th10",
+  th11: "th11",
+  th12: "th12",
+  th13: "th13",
+  th14: "th14",
+  th15: "th15"
+};
+
+const td = {
+  td1: "123 12:34",
+  td2: "情報デザイン",
+  td3: "Golf6",
+  td4: "-",
+  td5: "-",
+  td6: "",
+  td7: "-",
+  td8: "池澤",
+  td9: "-",
+  td10: "-",
+  td11_1: "-",
+  td12_1: "-",
+  td13_1: "-",
+  td11_2: "-",
+  td12_2: "-",
+  td13_2: "-",
+  td11_3: "-",
+  td12_3: "-",
+  td13_3: "-", 
+  td14: "",
+  td15: ""
+};
+
+function getDataHeading() {
+  return th;
+}
+
+function getDataValue() {
+  return td;
+}
---
+++ sample/form.js
@@ -0,0 +1,53 @@
+const medias = {audio : false, video : {
+        facingMode : {
+          exact : "environment"
+        }
+      }},
+      video  = document.getElementById("video"),
+      canvas = document.getElementById("canvas"),
+      ctx    = canvas.getContext("2d");
+
+function applyForm(head) {
+  for (var name in head) {
+    node = document.getElementById(name);
+    node.textContent = head[name];
+  }
+};
+
+applyForm(getDataHeading());
+applyForm(getDataValue());
+
+let imgData, data, ave;
+
+/* navigator.getUserMedia(medias, successCallback, errorCallback); */
+
+/* animation = requestAnimationFrame(draw); */
+
+function successCallback(stream) {
+  video.srcObject = stream;
+};
+
+function errorCallback(err) {
+  alert(err);
+};
+
+function draw() {
+  canvas.width  = window.innerWidth;
+  canvas.height = window.innerHeight;
+  ctx.drawImage(video, 0, 0);
+
+  imgData = ctx.getImageData(0, 0, canvas.width,  canvas.height);
+    data = imgData.data;
+
+    for (let i = 0; i < data.length; i += 4) {
+      ave = (data[i + 0] + data[i + 1] + data[i + 2]) / 3;
+
+      data[i + 0] = 
+      data[i + 1] = 
+      data[i + 2] = (ave > 255 / 2) ? 255 : (ave > 255 / 4) ? 127 : 0;
+      data[i + 3] = 255;
+    }
+
+  ctx.putImageData(imgData, 0, 0);
+  animation = requestAnimationFrame(draw);
+}
