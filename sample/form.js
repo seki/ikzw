bc68e66c66adf3fb999504cd459b52855ef7259f
Masatoshi SEKI <m_seki@mva.biglobe.ne.jp>
2018-06-30 08:19:16 +0900

    remove monocrome

--- sample/form.js
+++ sample/form.js
@@ -51,16 +51,6 @@ function draw() {
   ctx.drawImage(video, 0, 0);
 
   imgData = ctx.getImageData(0, 0, canvas.width,  canvas.height);
-    data = imgData.data;
-
-    for (let i = 0; i < data.length; i += 4) {
-      ave = (data[i + 0] + data[i + 1] + data[i + 2]) / 3;
-
-      data[i + 0] = 
-      data[i + 1] = 
-      data[i + 2] = (ave > 255 / 2) ? 255 : (ave > 255 / 4) ? 127 : 0;
-      data[i + 3] = 255;
-    }
 
   ctx.putImageData(imgData, 0, 0);
   memo = jsQR(imgData.data, imgData.width, imgData.height);
