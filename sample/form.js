6dc5bc9d1d250c0fd84f48ee48307b20130d9277
Masatoshi SEKI <m_seki@mva.biglobe.ne.jp>
2018-06-30 18:10:43 +0900

    create QR code

--- sample/form.js
+++ sample/form.js
@@ -28,12 +28,10 @@ function openQRMode() {
 }
 
 function openTableMode() {
+  prepareTicket(table, getDataHeading(), getDataValue());
   canvas.style.display = "none";
   table.style.display = "inline";
 }
-
-applyForm(table, getDataHeading());
-applyForm(table, getDataValue());
 
 function successCallback(stream) {
   count = 0;
@@ -65,4 +63,68 @@ function draw() {
     count = 0;
   }
   animation = requestAnimationFrame(draw);
+}
+
+function MyQR(options) {
+  // if options is string, 
+  if( typeof options === 'string' ){
+    options	= { text: options };
+  }
+
+  // set default values
+  // typeNumber < 1 for automatic calculation
+  options = options || {};
+  var default_options = {
+    width		: 64,
+    height		: 64,
+    typeNumber	: -1,
+    correctLevel	: QRErrorCorrectLevel.H,
+    background      : "#ffffff",
+    foreground      : "#000000"
+  };
+  for (var key in default_options){
+    if(!options[key]){
+      options[key] = default_options[key]
+    }
+  }
+
+  var createCanvas	= function(){
+    // create the qrcode itself
+    var qrcode	= new QRCode(options.typeNumber, options.correctLevel);
+    qrcode.addData(options.text);
+    qrcode.make();
+
+    // create canvas element
+    var canvas	= document.createElement('canvas');
+    canvas.width	= options.width;
+    canvas.height	= options.height;
+    var ctx		= canvas.getContext('2d');
+
+    // compute tileW/tileH based on options.width/options.height
+    var tileW	= options.width  / qrcode.getModuleCount();
+    var tileH	= options.height / qrcode.getModuleCount();
+
+    // draw in the canvas
+    for( var row = 0; row < qrcode.getModuleCount(); row++ ){
+      for( var col = 0; col < qrcode.getModuleCount(); col++ ){
+        ctx.fillStyle = qrcode.isDark(row, col) ? options.foreground : options.background;
+        var w = (Math.ceil((col+1)*tileW) - Math.floor(col*tileW));
+        var h = (Math.ceil((row+1)*tileW) - Math.floor(row*tileW));
+        ctx.fillRect(Math.round(col*tileW),Math.round(row*tileH), w, h);  
+      }	
+    }
+    // return just built canvas
+    return canvas;
+  }
+  return createCanvas();
+};
+
+function prepareTicket(ticket, heading, value) {
+  applyForm(ticket, heading) 
+  applyForm(ticket, value);
+  qr_canvas = MyQR(value.td1);
+
+  it = table.getElementsByClassName("qr")[0];
+  it.innerHTML = "";
+  it.appendChild(qr_canvas);
 }
